4496a1f00c0d9dd3d075b7b1c68f921147a2db99
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-05-11 02:55:16 +0300

    add imports

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, redirect, url_for,abort,request
 from . import main
-from flask_login import login_required
-from ..models import User
+from flask_login import login_required,current_user
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .form import UpdateProfile,PitchForm,CommentForm
+from .. import db,photos
 
 
 @main.route('/')
@@ -10,16 +12,84 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    
-    return render_template('index.html')
 
-@main.route('/profile')
+    pitches = Pitch.query.all()
+    Interviews = Pitch.query.filter_by(category = 'interviews').all() 
+    Business = Pitch.query.filter_by(category = 'Business').all()
+    enterpreneurship = Pitch.query.filter_by(category = 'enterpreneurship').all()
+
+
+
+    return render_template('index.html','Business=Business','interviews=interviews','enterpreneurship=enterpreneurship')
+
+    
+    
+@main.route('/create_new', methods = ['POST','GET'])
 @login_required
-def profile():
 
-    '''
-    View root page function that returns the profile page and its data
-    '''
-   
-    
-    return render_template('profile.html')
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user
+        new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
+        new_pitch_object.save_p()
+        return redirect(url_for('main.index'))
+        
+    return render_template('create_pitch.html', form = form)
+
+@main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
+@login_required
+
+def comment(pitch_id):
+    form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        pitch_id = pitch_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
+        new_comment.save_c()
+        return redirect(url_for('.comment', pitch_id = pitch_id))
+    return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+
+
+@main.route('/user/<name>')
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    user_id = current_user._get_current_object().id
+    posts = Pitch.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,posts=posts)
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save_u()
+        return redirect(url_for('.profile',name = name))
+    return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<name>/update/pic',methods= ['POST'])
+@login_required
+
+def update_pic(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',name=name))
